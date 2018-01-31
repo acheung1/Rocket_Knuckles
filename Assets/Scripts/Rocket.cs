df52becd97f7ad40979680583c96d941fd441d6a
acheung1 <andrewcheungmd@gmail.com>
2018-01-31 02:00:49 -0500

    added rocket audio and improve movement

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,29 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100f; // make this allow to be changed in the unity editor
+    [SerializeField] float mainThurst = 100f;
     Rigidbody rigidbody;
+    AudioSource rocketThrust;
 
 	// Use this for initialization
 	void Start () {
-        rigidbody = GetComponent<Rigidbody>();  
+        rigidbody = GetComponent<Rigidbody>();
+        rocketThrust = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
-
-    private void ProcessInput()
+    
+    private void Thrust()
     {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            rigidbody.AddRelativeForce(Vector3.up);
-        } else if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Rotate(Vector3.back);
+            rigidbody.AddRelativeForce(Vector3.up * mainThurst);
+            if (!rocketThrust.isPlaying)
+            {
+                rocketThrust.Play();
+            }
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else
         {
-            transform.Rotate(Vector3.forward);
+            rocketThrust.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.left * rotationThisFrame);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.right * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false; // resume physics control of rotation
     }
 }
